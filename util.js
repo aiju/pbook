e1cde272b16710a3863640d70274fc69da303d68
Julius Schmidt <aiju@phicode.de>
2016-12-10 14:28:35 +0100

    fix html and javascript

--- util.js
+++ util.js
@@ -1,9 +1,9 @@
 function show(e){
-	if(e.srcElement.parentNode.childNodes[1].style.display == "none"){
-		e.srcElement.parentNode.childNodes[1].style.display = 'block';
+	if(e.srcElement.nextSibling.style.display == "none"){
+		e.srcElement.nextSibling.style.display = 'block';
 		e.srcElement.innerHTML = 'hide';
 	}else{
-		e.srcElement.parentNode.childNodes[1].style.display = 'none';
+		e.srcElement.nextSibling.style.display = 'none';
 		e.srcElement.innerHTML = 'show';
 	}
 }
