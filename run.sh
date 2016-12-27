4295afbf57c0e746df99901e0f634f3bf2389d14
Julius Schmidt <aiju@phicode.de>
2016-12-27 14:27:10 +0100

    rewrite em section using complex operators

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p www
-htlatex index.tex tex4ht.cfg,2 "" "-d www/" "-interaction=nonstopmode"
+htlatex index.tex tex4ht.cfg,3 "" "-d www/" "-interaction=nonstopmode"
 cp util.js style.css www
